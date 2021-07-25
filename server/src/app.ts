ab9ac021fae073c807963ce39d52b5d058b7078b
jeffascript <ofoaro.jeff@gmail.com>
2021-07-25 14:24:09 +0200

    [server] app basic config initialised

---
+++ server/src/app.ts
@@ -0,0 +1,26 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+import cors from 'cors';
+
+class App {
+    public app: Application;
+
+    constructor() {
+        this.app = express();
+        this.config();
+    }
+
+    private config(): void {
+        this.app.use((req: Request, res: Response, next: NextFunction) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT');
+            res.header('Access-Control-Allow-Headers', '*');
+            next();
+        });
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: false }));
+    }
+}
+
+export default new App().app;
