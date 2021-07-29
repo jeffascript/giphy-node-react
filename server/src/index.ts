41a56e6f6512b3de024b4799baf4429ff94b1c6e
jeffascript <ofoaro.jeff@gmail.com>
2021-07-29 13:54:56 +0200

    port updated

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,9 @@ import app from './app';
 
 dotenv.config();
 
-const port = parseInt(<string>process.env.PORT, 10) || 5000;
+const port = process.env.PORT || 5001;
+
+// const port = parseInt(<string>process.env.PORT, 10) || 5000;
 
 app.listen(port, () => {
     console.log(`App Started on ${port}`);
